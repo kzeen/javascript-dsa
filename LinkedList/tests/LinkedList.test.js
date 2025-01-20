33b203e35d77c54c8e177762ec2be59238954e32
kzeen <157370421+kzeen@users.noreply.github.com>
2025-01-21 00:21:45 +0200

    Add LinkedList tests

--- LinkedList/tests/LinkedList.test.js
+++ LinkedList/tests/LinkedList.test.js
@@ -1,78 +1,140 @@
 const LinkedList = require("../LinkedList");
 
+describe("LinkedList tests", () => {
+    it("should create an empty LinkedList", () => {
+        const linkedlist = new LinkedList();
 
-function testAppend() {
-    const test = new LinkedList();
-    
-    test.append(1);
-    test.append(2)
-    test.append(3)
+        expect(linkedlist.head).toBeNull();
+        expect(linkedlist.tail).toBeNull();
+        expect(linkedlist.toString()).toBe('');
+    })
 
-    console.log(test)
-}
+    it("should add nodes to the end of the LinkedList", () => {
+        const linkedlist = new LinkedList();
 
-function testPrepend() {
-    const test = new LinkedList();
+        linkedlist.append(1);
+        expect(linkedlist.head).toBeDefined();
+        expect(linkedlist.tail).toBeDefined();
+        expect(linkedlist.head).toEqual(linkedlist.tail);
+        expect(linkedlist.toString()).toBe('1');
 
-    test.prepend(1);
-    test.prepend(2);
-    test.prepend(3);
+        linkedlist.append(2);
+        expect(linkedlist.head.value).toBe(1);
+        expect(linkedlist.head.next).toBe(linkedlist.tail);
+        expect(linkedlist.tail.value).toBe(2);
+        expect(linkedlist.tail.next).toBeNull();
+        expect(linkedlist.head).not.toEqual(linkedlist.tail);
+        expect(linkedlist.toString()).toBe('1,2');
+    })
 
-    console.log(test);
-}
+    it("should add nodes to the beginning of the LinkedList", () => {
+        const linkedlist = new LinkedList();
 
-function testContains() {
-    const test = new LinkedList();
+        linkedlist.prepend(1);
+        expect(linkedlist.toString()).toBe('1');
+        expect(linkedlist.head.next).toBeNull();
 
-    console.log(test.contains(1));
+        linkedlist.prepend(2);
+        expect(linkedlist.head.value).toBe(2);
+        expect(linkedlist.tail.value).toBe(1);
+        expect(linkedlist.head.next).toEqual(linkedlist.tail);
+        expect(linkedlist.toString()).toBe('2,1');
 
-    test.append(1);
-    test.prepend(2);
-    test.prepend(123);
-    test.append(123213);
+        linkedlist.prepend(3);
+        linkedlist.append(4);
+        linkedlist.prepend(5)
+        expect(linkedlist.toString()).toBe('5,3,2,1,4');
+    })
 
-    console.log(test.contains(10));
-    console.log(test.contains(123));
-}
+    it("should return whether LinkedList contains value", () => {
+        const linkedlist = new LinkedList();
 
-function testDelete() {
-    const test = new LinkedList();
+        expect(linkedlist.contains(1)).toBeFalsy();
 
-    test.append(1);
-    test.append(2);
-    test.append(3);
+        linkedlist.append(10);
+        linkedlist.prepend(2);
+        expect(linkedlist.contains(24)).toBeFalsy();
+        expect(linkedlist.contains(2)).toBeTruthy();
+        expect(linkedlist.contains(10)).toBeTruthy();
 
-    test.traverse()
-    console.log(test.delete(3));
-    test.traverse()
-}
+        linkedlist.prepend(100);
+        linkedlist.append(100);
+        expect(linkedlist.contains(100)).toBeTruthy();
+    })
 
-function testTraverse() {
-    const test = new LinkedList();
+    it("should delete node from LinkedList", () => {
+        const linkedlist = new LinkedList();
 
-    test.append(1);
-    test.append(2);
-    test.append(3);
+        expect(linkedlist.delete(123)).toBeFalsy();
 
-    test.traverse();
-}
+        linkedlist.append(123);
+        linkedlist.prepend(321);
+        linkedlist.append(2);
+        expect(linkedlist.toString()).toBe('321,123,2');
+        expect(linkedlist.delete(123)).toBeTruthy();
+        expect(linkedlist.toString()).toBe('321,2');
 
-function testReverseTraverse() {
-    const test = new LinkedList();
-    console.log(test.toArray().toString() === '');
+        linkedlist.append(10);
+        expect(linkedlist.delete(321)).toBeTruthy();
+        expect(linkedlist.head.value).toBe(2);
+        expect(linkedlist.toString()).toBe('2,10');
+        expect(linkedlist.delete(21)).toBeFalsy();
 
-    test.append(1);
-    test.append(2);
-    test.append(3);
-    test.append(4);
+        linkedlist.append(10);
+        expect(linkedlist.delete(10)).toBeTruthy();
+        expect(linkedlist.toString()).toBe('2,10');
+        expect(linkedlist.delete(10)).toBeTruthy();
+        expect(linkedlist.tail.value).toBe(2);
+        expect(linkedlist.toString()).toBe('2');
+        expect(linkedlist.delete(2)).toBeTruthy();
+        expect(linkedlist.head).toBeNull();
+    })
 
-    test.reverseTraverse();
-    console.log(test.toArray().toString());
-}
+    it("should traverse the LinkedList in order", () => {
+        const linkedlist = new LinkedList();
 
-// testAppend();
-// testPrepend();
-// testContains();
-// testDelete();
-// testTraverse();
-testReverseTraverse();
+        expect(linkedlist.traverse()).toBe('');
+
+        linkedlist.append(1);
+        expect(linkedlist.traverse()).toBe('1');
+
+        linkedlist.append(2);
+        linkedlist.append(3);
+        expect(linkedlist.traverse()).toBe('1,2,3');
+
+        linkedlist.prepend(4);
+        linkedlist.prepend(5);
+        linkedlist.append(6);
+        expect(linkedlist.traverse()).toBe('5,4,1,2,3,6');
+
+        expect(linkedlist.delete(5)).toBeTruthy();
+        expect(linkedlist.traverse()).toBe('4,1,2,3,6');
+        expect(linkedlist.delete(2)).toBeTruthy();
+        linkedlist.append(10);
+        expect(linkedlist.traverse()).toBe('4,1,3,6,10');
+    })
+
+    it("should traverse the LinkedList in reverse order", () => {
+        const linkedlist = new LinkedList();
+
+        expect(linkedlist.reverseTraverse()).toBe('');
+
+        linkedlist.append(1);
+        expect(linkedlist.reverseTraverse()).toBe('1');
+
+        linkedlist.append(2);
+        linkedlist.append(3);
+        expect(linkedlist.reverseTraverse()).toBe('3,2,1');
+
+        linkedlist.prepend(4);
+        linkedlist.prepend(5);
+        linkedlist.append(6);
+        expect(linkedlist.reverseTraverse()).toBe('6,3,2,1,4,5');
+
+        expect(linkedlist.delete(5)).toBeTruthy();
+        expect(linkedlist.reverseTraverse()).toBe('6,3,2,1,4');
+        expect(linkedlist.delete(2)).toBeTruthy();
+        linkedlist.append(10);
+        expect(linkedlist.reverseTraverse()).toBe('10,6,3,1,4');
+    })
+})
